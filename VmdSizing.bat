825e66a9edb023f66b1c4aa0f55ad6b5bf65c3d0
miu <garnet200521358@gmail.com>
2019-04-28 13:48:18 +0900

    頂点CSV指定項目追加

--- VmdSizing.bat
+++ VmdSizing.bat
@@ -35,11 +35,11 @@ set /P REPLACE_MODEL_BONE_CSV="■トレース変換先モデルボーン構造CSVファイル: "
 
 
 rem rem ---  トレース変換先モデル頂点構造CSVファイル
-rem echo --------------
+echo --------------
 set REPLACE_MODEL_VERTEX_CSV=%REPLACE_MODEL_BONE_CSV:born=vertex%
-rem echo トレース変換先モデルの頂点構造CSVファイルの相対パスを入力して下さい。
-rem echo 何も入力せず、ENTERを押下した場合、「%REPLACE_MODEL_VERTEX_CSV%」のファイルを読み込みます。
-rem set /P REPLACE_MODEL_VERTEX_CSV="■トレース変換先モデル頂点構造CSVファイル: "
+echo トレース変換先モデルの頂点構造CSVファイルの相対パスを入力して下さい。
+echo 何も入力せず、ENTERを押下した場合、「%REPLACE_MODEL_VERTEX_CSV%」のファイルを読み込みます。
+set /P REPLACE_MODEL_VERTEX_CSV="■トレース変換先モデル頂点構造CSVファイル: "
 
 
 rem ---  詳細ログ有無
@@ -48,7 +48,7 @@ echo --------------
 echo 詳細なログを出すか、yes か no を入力して下さい。
 echo 何も入力せず、ENTERを押下した場合、通常ログを出力します。
 set VERBOSE=2
-set IS_DEBUG=yes
+set IS_DEBUG=no
 set /P IS_DEBUG="■詳細ログ[yes/no]: "
 
 IF /I "%IS_DEBUG%" EQU "yes" (
