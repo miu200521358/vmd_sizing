a04b44d3533bd842476c3539f04007ecba7699a2
miu <garnet200521358@gmail.com>
2019-07-31 16:21:12 +0900

    コメント追加

--- hook/hook-PyQt5.QtGui.py
+++ hook/hook-PyQt5.QtGui.py
@@ -6,6 +6,9 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
+# exeサイズ削減のため、binariesをコンパイル対象に含めない
+# "<Anacondaインストールパス>\envs\vmdsizing_inst_32\Lib\site-packages\PyInstaller\hooks\hook-PyQt5.QtGui.py"に配置
+# "<Anacondaインストールパス>\envs\vmdsizing_inst_64\Lib\site-packages\PyInstaller\hooks\hook-PyQt5.QtGui.py"に配置
 from PyInstaller.utils.hooks import add_qt5_dependencies
 
 hiddenimports, binaries, datas = add_qt5_dependencies(__file__)
