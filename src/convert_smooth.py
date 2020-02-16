ac410581cdde619e05ee13c330474eae1973a7cb
miu200521358 <garnet200521358@gmail.com>
2020-02-16 20:10:11 +0900

    ログ追加

--- src/convert_smooth.py
+++ src/convert_smooth.py
@@ -357,6 +357,19 @@ def delegate_twist_qq_4_arm(fno, direction, arm_qq, arm_twist_qq, elbow_qq, wris
     elbow_x_qq, elbow_y_qq, elbow_z_qq, elbow_yz_qq = utils.split_qq_2_xyz(fno, "{0}ひじ".format(direction), elbow_qq, elbow_local_x_axis)
     wrist_x_qq, wrist_y_qq, wrist_z_qq, wrist_yz_qq = utils.split_qq_2_xyz(fno, "{0}手首".format(direction), wrist_qq, wrist_local_x_axis)
 
+    utils.output_file_logger(file_logger, "fno: {fno}, arm_x_qq: {arm_x_qq}".format(fno=fno, arm_x_qq=arm_x_qq.toEulerAngles()), level=logging.DEBUG)
+    utils.output_file_logger(file_logger, "fno: {fno}, arm_y_qq: {arm_y_qq}".format(fno=fno, arm_y_qq=arm_y_qq.toEulerAngles()), level=logging.DEBUG)
+    utils.output_file_logger(file_logger, "fno: {fno}, arm_z_qq: {arm_z_qq}".format(fno=fno, arm_z_qq=arm_z_qq.toEulerAngles()), level=logging.DEBUG)
+
+    utils.output_file_logger(file_logger, "fno: {fno}, elbow_x_qq: {elbow_x_qq}".format(fno=fno, elbow_x_qq=elbow_x_qq.toEulerAngles()), level=logging.DEBUG)
+    utils.output_file_logger(file_logger, "fno: {fno}, elbow_y_qq: {elbow_y_qq}".format(fno=fno, elbow_y_qq=elbow_y_qq.toEulerAngles()), level=logging.DEBUG)
+    utils.output_file_logger(file_logger, "fno: {fno}, elbow_z_qq: {elbow_z_qq}".format(fno=fno, elbow_z_qq=elbow_z_qq.toEulerAngles()), level=logging.DEBUG)
+
+    utils.output_file_logger(file_logger, "fno: {fno}, wrist_x_qq: {wrist_x_qq}".format(fno=fno, wrist_x_qq=wrist_x_qq.toEulerAngles()), level=logging.DEBUG)
+    utils.output_file_logger(file_logger, "fno: {fno}, wrist_y_qq: {wrist_y_qq}".format(fno=fno, wrist_y_qq=wrist_y_qq.toEulerAngles()), level=logging.DEBUG)
+    utils.output_file_logger(file_logger, "fno: {fno}, wrist_z_qq: {wrist_z_qq}".format(fno=fno, wrist_z_qq=wrist_z_qq.toEulerAngles()), level=logging.DEBUG)
+
+
     # 腕YZを腕に
     arm_result_qq = arm_yz_qq
 
@@ -386,19 +399,19 @@ def delegate_twist_qq_4_arm(fno, direction, arm_qq, arm_twist_qq, elbow_qq, wris
     arm_twist_result_qq = utils.delegate_twist_qq(fno, "{0}腕捩".format(direction), arm_qq, arm_result_qq, arm_twist_qq, arm_twist_result_qq, elbow_qq, elbow_result_qq, \
         arm_twist_axis_degree, arm_local_x_axis, arm_twist_local_x_axis, elbow_local_x_axis, file_logger)
 
-    # # 手首ローカル軸 
-    # wrist_local_y_axis = QVector3D.crossProduct(wrist_local_x_axis, QVector3D(0, 0, 1)).normalized()
-    # wrist_local_z_axis = QVector3D.crossProduct(wrist_local_x_axis, wrist_local_y_axis).normalized()
+    # 手首ローカル軸 
+    wrist_local_y_axis = QVector3D.crossProduct(wrist_local_x_axis, QVector3D(0, 0, 1)).normalized()
+    wrist_local_z_axis = QVector3D.crossProduct(wrist_local_x_axis, wrist_local_y_axis).normalized()
 
-    # wrist_y_axis_qq, wrist_y_axis_degree = utils.convert_axis_qq(fno, "{0}手首Y".format(direction), wrist_y_qq, wrist_local_x_axis, wrist_local_y_axis, "y", file_logger)
-    # wrist_z_axis_qq, wrist_z_axis_degree = utils.convert_axis_qq(fno, "{0}手首Z".format(direction), wrist_z_qq, wrist_local_x_axis, wrist_local_z_axis, "z", file_logger)
+    wrist_y_axis_qq, wrist_y_axis_degree = utils.convert_axis_qq(fno, "{0}手首Y".format(direction), wrist_y_qq, wrist_local_x_axis, wrist_local_y_axis, "y", file_logger)
+    wrist_z_axis_qq, wrist_z_axis_degree = utils.convert_axis_qq(fno, "{0}手首Z".format(direction), wrist_z_qq, wrist_local_x_axis, wrist_local_z_axis, "z", file_logger)
 
-    # # 手首XYZ回転を手首YZにする
-    # wrist_result_qq = wrist_z_axis_qq * wrist_y_axis_qq
+    # 手首XYZ回転を手首YZにする
+    wrist_result_qq = wrist_z_axis_qq * wrist_y_axis_qq
 
-    # # 手捩りで手首ベクトル帳尻合わせ（ここでだいたい整合するか近似する）
-    # wrist_twist_result_qq = utils.delegate_twist_qq(fno, "{0}手捩".format(direction), arm_twist_qq, arm_twist_result_qq, wrist_twist_qq, QQuaternion(), wrist_qq, wrist_result_qq, \
-    #     0, arm_twist_local_x_axis, wrist_twist_local_x_axis, wrist_local_x_axis, file_logger)
+    # 手捩りで手首ベクトル帳尻合わせ（ここでだいたい整合するか近似する）
+    wrist_twist_result_qq = utils.delegate_twist_qq(fno, "{0}手捩".format(direction), arm_twist_qq, arm_twist_result_qq, wrist_twist_qq, QQuaternion(), wrist_qq, wrist_result_qq, \
+        0, arm_twist_local_x_axis, wrist_twist_local_x_axis, wrist_local_x_axis, file_logger)
 
     return arm_result_qq, arm_twist_result_qq, elbow_result_qq, wrist_twist_result_qq, wrist_result_qq
 
