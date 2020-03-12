40b85d461bf0292831245f2fe78c4f59b683d2fe
miu200521358 <garnet200521358@gmail.com>
2020-03-12 23:05:51 +0900

    ver4.06_β12
    VMDサイジング ver4.06_β12
    ・VMD変換時、補間曲線等整数のみ受け付けるパラメーターは、指数表記等の対応の為、一旦floatで読み込んだ後にint変換する

--- src/convert_vmd.py
+++ src/convert_vmd.py
@@ -43,7 +43,7 @@ def main(csv_bone_path, csv_morph_path, csv_camera_path):
                     bf.name = bf.format_name.encode('cp932').decode('shift_jis').encode('shift_jis')
 
                     # フレーム
-                    bf.frame = int(row[1])
+                    bf.frame = int(float(row[1]))
 
                     # 位置
                     bf.position = QVector3D(float(row[2]), float(row[3]), float(row[4]))
@@ -51,8 +51,8 @@ def main(csv_bone_path, csv_morph_path, csv_camera_path):
                     # 回転
                     bf.rotation = QQuaternion.fromEulerAngles(float(row[5]), float(row[6])*-1, float(row[7])*-1)
 
-                    # 補間曲線
-                    bf.complement = [int(row[8]), int(row[9]), int(row[10]),int(row[11]),int(row[12]),int(row[13]),int(row[14]),int(row[15]),int(row[16]),int(row[17]),int(row[18]),int(row[19]),int(row[20]),int(row[21]),int(row[22]),int(row[23]),int(row[24]),int(row[25]),int(row[26]),int(row[27]),int(row[28]),int(row[29]),int(row[30]),int(row[31]),int(row[32]),int(row[33]),int(row[34]),int(row[35]),int(row[36]),int(row[37]),int(row[38]),int(row[39]),int(row[40]),int(row[41]),int(row[42]),int(row[43]),int(row[44]),int(row[45]),int(row[46]),int(row[47]),int(row[48]),int(row[49]),int(row[50]),int(row[51]),int(row[52]),int(row[53]),int(row[54]),int(row[55]),int(row[56]),int(row[57]),int(row[58]),int(row[59]),int(row[60]),int(row[61]),int(row[62]),int(row[63]),int(row[64]),int(row[65]),int(row[66]),int(row[67]),int(row[68]),int(row[69]),int(row[70]),int(row[71])]
+                    # 補間曲線(一旦floatで読み込んで指数等も読み込んだ後、intに変換)
+                    bf.complement = [int(float(row[8])), int(float(row[9])), int(float(row[10])),int(float(row[11])),int(float(row[12])),int(float(row[13])),int(float(row[14])),int(float(row[15])),int(float(row[16])),int(float(row[17])),int(float(row[18])),int(float(row[19])),int(float(row[20])),int(float(row[21])),int(float(row[22])),int(float(row[23])),int(float(row[24])),int(float(row[25])),int(float(row[26])),int(float(row[27])),int(float(row[28])),int(float(row[29])),int(float(row[30])),int(float(row[31])),int(float(row[32])),int(float(row[33])),int(float(row[34])),int(float(row[35])),int(float(row[36])),int(float(row[37])),int(float(row[38])),int(float(row[39])),int(float(row[40])),int(float(row[41])),int(float(row[42])),int(float(row[43])),int(float(row[44])),int(float(row[45])),int(float(row[46])),int(float(row[47])),int(float(row[48])),int(float(row[49])),int(float(row[50])),int(float(row[51])),int(float(row[52])),int(float(row[53])),int(float(row[54])),int(float(row[55])),int(float(row[56])),int(float(row[57])),int(float(row[58])),int(float(row[59])),int(float(row[60])),int(float(row[61])),int(float(row[62])),int(float(row[63])),int(float(row[64])),int(float(row[65])),int(float(row[66])),int(float(row[67])),int(float(row[68])),int(float(row[69])),int(float(row[70])),int(float(row[71]))]
 
                     bone_frames.append(bf)
                 
@@ -76,7 +76,7 @@ def main(csv_bone_path, csv_morph_path, csv_camera_path):
                     mf.name = mf.format_name.encode('cp932').decode('shift_jis').encode('shift_jis')
 
                     # フレーム
-                    mf.frame = int(row[1])
+                    mf.frame = int(float(row[1]))
 
                     # 位置
                     mf.ratio = float(row[2])
@@ -101,7 +101,7 @@ def main(csv_bone_path, csv_morph_path, csv_camera_path):
                     cf = VmdCameraFrame()
 
                     # フレーム
-                    cf.frame = int(row[0])
+                    cf.frame = int(float(row[0]))
 
                     # 位置
                     cf.position = QVector3D(float(row[1]), float(row[2]), float(row[3]))
@@ -113,13 +113,13 @@ def main(csv_bone_path, csv_morph_path, csv_camera_path):
                     cf.length = -(float(row[7]))
 
                     # 視野角
-                    cf.angle = int(row[8])
+                    cf.angle = int(float(row[8]))
 
                     # パース
-                    cf.perspective = int(row[9])
+                    cf.perspective = int(float(row[9]))
 
                     # 補間曲線
-                    cf.complement = [int(row[10]),int(row[11]),int(row[12]),int(row[13]),int(row[14]),int(row[15]),int(row[16]),int(row[17]),int(row[18]),int(row[19]),int(row[20]),int(row[21]),int(row[22]),int(row[23]),int(row[24]),int(row[25]),int(row[26]),int(row[27]),int(row[28]),int(row[29]),int(row[30]),int(row[31]),int(row[32]),int(row[33])]
+                    cf.complement = [int(float(row[10])),int(float(row[11])),int(float(row[12])),int(float(row[13])),int(float(row[14])),int(float(row[15])),int(float(row[16])),int(float(row[17])),int(float(row[18])),int(float(row[19])),int(float(row[20])),int(float(row[21])),int(float(row[22])),int(float(row[23])),int(float(row[24])),int(float(row[25])),int(float(row[26])),int(float(row[27])),int(float(row[28])),int(float(row[29])),int(float(row[30])),int(float(row[31])),int(float(row[32])),int(float(row[33]))]
 
                     camera_frames.append(cf)
                 
