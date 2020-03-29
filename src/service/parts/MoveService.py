51240ce1ddff9d7185bfc1a3237b83282ff5befc
miu200521358 <garnet200521358@gmail.com>
2020-03-29 22:20:48 +0900

    足IKオフセット追加

--- src/service/parts/MoveService.py
+++ src/service/parts/MoveService.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-
+import numpy as np
 from module.MMath import MRect, MVector3D, MVector4D, MQuaternion, MMatrix4x4 # noqa
 from module.MOptions import MOptions, MOptionsDataSet
 from utils import MUtils, MServiceUtils # noqa
@@ -24,6 +24,9 @@ class MoveService():
             # センターのZ軸オフセットを計算
             self.set_center_z_offset(data_set)
 
+            # 足IKのオフセットを計算
+            self.set_leg_ik_offset(data_set)
+
             for k in ["右足ＩＫ", "左足ＩＫ", "右つま先ＩＫ", "左つま先ＩＫ", "右足ＩＫ親", "左足ＩＫ親", "右足IK親", "左足IK親", "センター", "グルーブ", "全ての親"]:
                 if k in data_set.motion.bones and k in data_set.rep_model.bones:
                     for bf in data_set.motion.bones[k].values():
@@ -39,6 +42,43 @@ class MoveService():
                         logger.info("移動補正: %s", k)
 
         return True
+    
+    def set_leg_ik_offset(self, data_set: MOptionsDataSet):
+        target_bones = ["左足", "左足ＩＫ", "右足ＩＫ"]
+
+        if set(target_bones).issubset(data_set.org_model.bones) and set(target_bones).issubset(data_set.rep_model.bones):
+            # 足ボーンのY差
+            leg_ratio = data_set.rep_model.bones["左足"].position.y() / data_set.org_model.bones["左足"].position.y()
+
+            # 足IKのオフセット上限
+            leg_ik_offset = {"左": MVector3D(), "右": MVector3D()}
+            IK_RATE = data_set.original_xz_ratio * 1.2
+            for direction in ["左", "右"]:
+                org_leg_ik_pos = data_set.org_model.bones["{0}足ＩＫ".format(direction)].position
+                rep_leg_ik_pos = data_set.rep_model.bones["{0}足ＩＫ".format(direction)].position
+                org_leg_pos = data_set.org_model.bones["{0}足".format(direction)].position
+                rep_leg_pos = data_set.rep_model.bones["{0}足".format(direction)].position
+                # IKオフセット
+                leg_ik_offset[direction] = ((org_leg_ik_pos - org_leg_pos) * leg_ratio) - (rep_leg_ik_pos - rep_leg_pos)
+                leg_ik_offset[direction].effective()
+                leg_ik_offset[direction].setY(0)
+                logger.test("leg_ik_offset(%s): %s", direction, leg_ik_offset[direction])
+
+                if abs(leg_ik_offset[direction].x()) > abs(rep_leg_ik_pos.x() * IK_RATE):
+                    # IKオフセットが、元々の足の位置の一定以上に広がっている場合、縮める
+                    re_x = rep_leg_ik_pos.x() * IK_RATE
+                    # オフセットの広がり具合が、元々と同じ場合は正、反対の場合、負
+                    leg_ik_offset[direction].setX(re_x * (1 if np.sign(leg_ik_offset[direction].x()) == np.sign(rep_leg_ik_pos.x()) else -1))
+
+            logger.info("IKオフセット(%s): x: %s, z: %s", "左足", leg_ik_offset["左"].x(), leg_ik_offset["左"].z())
+            logger.info("IKオフセット(%s): x: %s, z: %s", "右足", leg_ik_offset["右"].x(), leg_ik_offset["右"].z())
+
+            data_set.rep_model.bones["左足ＩＫ"].local_offset = leg_ik_offset["左"]
+            data_set.rep_model.bones["右足ＩＫ"].local_offset = leg_ik_offset["右"]
+
+            return
+
+        logger.info("IKオフセットなし")
 
     # センターZオフセット計算
     def set_center_z_offset(self, data_set: MOptionsDataSet):
